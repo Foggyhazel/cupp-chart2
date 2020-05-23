d17265ef46d5574b964620406e55781b17da8988
chaipat_p <chaipat_p@live.com>
2020-05-23 23:32:52 +0700

    no stack on line chart

--- src/components/chart/LineChart.jsx
+++ src/components/chart/LineChart.jsx
@@ -3,7 +3,7 @@ import { useChartContext } from "./Chart";
 import { line as d3Line } from "d3-shape";
 import { Path } from "react-native-svg";
 
-export default function LineChart({ stack = false, ...override }) {
+export default function LineChart({ ...override }) {
   const { data, ys, y, xs, x } = useChartContext({ ...override });
   const line = d3Line().x((d) => xs(x(d)));
   return (
