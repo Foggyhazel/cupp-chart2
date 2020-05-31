99ac58502f404762b78d376c2457aa0e7a3f3615
chaipat_p <chaipat_p@live.com>
2020-05-31 20:30:33 +0700

    custom scatter dot

--- src/components/chart/ScatterPlot.jsx
+++ src/components/chart/ScatterPlot.jsx
@@ -3,7 +3,17 @@ import { Circle, G } from "react-native-svg";
 import { compose } from "./manager/scaleManager";
 import { CommonPlotConfigure } from "./selectors";
 
-function ScatterPlot({ data, x, y, xAxis, yAxis, scale, radius = 1 }) {
+function ScatterPlot({
+  data,
+  x,
+  y,
+  xAxis,
+  yAxis,
+  scale,
+  radius = 1,
+  color = "black",
+  opacity = 1,
+}) {
   const sx = scale(xAxis, "h");
   const sy = scale(yAxis, "v");
 
@@ -11,7 +21,14 @@ function ScatterPlot({ data, x, y, xAxis, yAxis, scale, radius = 1 }) {
     <G clipPath="url(#clip)">
       {[...y.values()].map((yi, iy) =>
         data.map((d, i) => (
-          <Circle key={`${iy}-${i}`} cx={sx(x(d))} cy={sy(yi(d))} r={radius} />
+          <Circle
+            key={`${iy}-${i}`}
+            cx={sx(x(d))}
+            cy={sy(yi(d))}
+            r={radius}
+            fill={color}
+            fillOpacity={opacity}
+          />
         ))
       )}
     </G>
