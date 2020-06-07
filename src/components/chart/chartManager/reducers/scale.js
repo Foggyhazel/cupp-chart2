86fb076a311cb650f8d1b48d3643ec2e8780c4c7
chaipat_p <chaipat_p@live.com>
2020-06-07 13:18:46 +0700

    rename state field

--- src/components/chart/chartManager/reducers/scale.js
+++ src/components/chart/chartManager/reducers/scale.js
@@ -7,24 +7,24 @@ const SCALE_EXPORT = chEXPORT(CH);
 const SCALE_UNEXPORT = chUNEXPORT(CH);
 const SCALE_COMMIT = chCOMMIT(CH);
 
-function scale(state = { getScales: {}, map: {} }, action) {
+function scale(state = { exportedScale: {}, map: {} }, action) {
   switch (action.type) {
     case SCALE_EXPORT:
       console.log("EXPORT");
       return {
         ...state,
-        getScales: { ...state.getScales, [action.key]: action.data },
+        exportedScale: { ...state.exportedScale, [action.key]: action.data },
       };
     case SCALE_UNEXPORT: {
       // eslint-disable-next-line no-unused-vars
-      const { [action.key]: omit, ...rest } = state.getScales;
+      const { [action.key]: omit, ...rest } = state.exportedScale;
       return {
         ...state,
-        getScales: rest,
+        exportedScale: rest,
       };
     }
     case SCALE_COMMIT:
-      console.log("COMMIT", state.getScales);
+      console.log("COMMIT", state.exportedScale);
       return state;
     default:
       return state;
@@ -32,3 +32,7 @@ function scale(state = { getScales: {}, map: {} }, action) {
 }
 
 export default scale;
+
+function finalizeScale(exportedScale) {
+  return {};
+}
