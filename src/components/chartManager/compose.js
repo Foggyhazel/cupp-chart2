42ecf653ebda5887f96f16251c1ae2dead15f890
Foggyhazel <foggyhazel@outlook.com>
2020-06-09 20:54:45 +0700

    rename connector's props from Elem to Plot

--- src/components/chartManager/compose.js
+++ src/components/chartManager/compose.js
@@ -43,7 +43,7 @@ const compose = (config) => (Plot) => {
         })}
         {Connector ? (
           <Connector
-            Elem={Plot}
+            Plot={Plot}
             innerProps={{ ...ownProps, ...setProps, data }}
           />
         ) : (
