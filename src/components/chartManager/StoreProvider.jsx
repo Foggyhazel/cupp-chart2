54d09ef85bbdd7723b3086b3a379aa02693ada38
Foggyhazel <foggyhazel@outlook.com>
2020-06-09 20:59:31 +0700

    Plug to redux-dev-tool extension

--- src/components/chartManager/StoreProvider.jsx
+++ src/components/chartManager/StoreProvider.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import { Provider } from "react-redux";
-import { combineReducers, createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import scale from "./reducers/scale";
 import { createChannelMiddleware } from "./channel";
 
 const rootReducer = combineReducers({ scale });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function StoreProvider({ children }) {
   const [store] = useState(() =>
-    createStore(rootReducer, applyMiddleware(createChannelMiddleware()))
+    //createStore(rootReducer, applyMiddleware(createChannelMiddleware()))
+    createStore(
+      rootReducer,
+      /* preloadedState, */ composeEnhancers(
+        applyMiddleware(createChannelMiddleware())
+      )
+    )
   );
   return <Provider store={store}>{children}</Provider>;
 }
