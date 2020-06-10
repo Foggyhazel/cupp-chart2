76355993cb0612903b9ce1060483ec5b6faedc01
Foggyhazel <foggyhazel@outlook.com>
2020-06-10 16:55:25 +0700

    add ForwardScale to pass  scale() and skip render

--- src/components/configure/connectors.jsx
+++ src/components/configure/connectors.jsx
@@ -3,16 +3,54 @@ import { connect } from "react-redux";
 import { useChartContext } from "../chartManager/chartContext";
 import { createSelector } from "reselect";
 
+const ForwardScale = ({ scaleDict, Plot, ...innerProps }) => {
+  const ctx = useChartContext();
+
+  const scale = useCallback(
+    (id, orient) => {
+      if (id == null) return null;
+      if (scaleDict == null) return null;
+
+      const s = (scaleDict[id] && scaleDict[id].scale) || null;
+      if (s) {
+        const _s = s.copy();
+        const {
+          margin: { left, right, bottom, top },
+          width,
+          height,
+        } = ctx;
+        if (orient === "h") _s.range([left, width - right]);
+        if (orient === "v") _s.range([height - bottom, top]);
+        _s.scaleType = scaleDict[id].scaleType;
+        _s.option = scaleDict[id].option;
+        return _s;
+      }
+      return null;
+    },
+    [ctx, scaleDict]
+  );
+
+  if (!scaleDict) return null;
+
+  console.log(
+    "%c render ",
+    "background: orange; color: black; border-radius: 4px",
+    `<${Plot.name} ${innerProps.id || ""}>`
+  );
+
+  return <Plot {...innerProps} scale={scale} />;
+};
+
 const getXAxis = (_, props) => props.xAxis;
 const getYAxis = (_, props) => props.yAxis;
 const getXScale = (state, props) => state.scale.map.get(props.xAxis);
 const getYScale = (state, props) => state.scale.map.get(props.yAxis);
 
-function makeScaleXYSelector() {
+function makeSelectScaleByXY() {
   return createSelector(
     [getXAxis, getYAxis, getXScale, getYScale],
     (xAxis, yAxis, xScale, yScale) => {
-      if (!xScale || !yScale) return null;
+      if (!(xAxis && xScale) && !(yAxis && yScale)) return null;
       return {
         [xAxis]: xScale,
         [yAxis]: yScale,
@@ -21,49 +59,28 @@ function makeScaleXYSelector() {
   );
 }
 
-function makeMapStateToProps() {
-  const scaleSelector = makeScaleXYSelector();
-  return (state, props) => ({
-    scaleDict: scaleSelector(state, props),
+export const getID = (_, props) => (props.X ? "_x" : props.Y ? "_y" : props.id);
+const getScale = (state, props) =>
+  state.scale.map.get(props.X ? "_x" : props.Y ? "_y" : props.id);
+function makeSelectScaleByID() {
+  return createSelector([getID, getScale], (id, scale) => {
+    if (!scale) return null;
+    return {
+      [id]: scale,
+    };
   });
 }
 
-export const PassScaleXY = connect(makeMapStateToProps)(
-  ({ scaleDict, Plot, ...innerProps }) => {
-    const ctx = useChartContext();
+export const PassScaleXY = connect(() => {
+  const scaleSelector = makeSelectScaleByXY();
+  return (state, props) => ({
+    scaleDict: scaleSelector(state, props),
+  });
+})(ForwardScale);
 
-    const scale = useCallback(
-      (id, orient) => {
-        if (id == null) return null;
-        if (scaleDict == null) return null;
-
-        const s = (scaleDict[id] && scaleDict[id].scale) || null;
-        if (s) {
-          const _s = s.copy();
-          const {
-            margin: { left, right, bottom, top },
-            width,
-            height,
-          } = ctx;
-          if (orient === "h") _s.range([left, width - right]);
-          if (orient === "v") _s.range([height - bottom, top]);
-          _s.scaleType = scaleDict[id].scaleType;
-          _s.option = scaleDict[id].option;
-          return _s;
-        }
-        return null;
-      },
-      [ctx, scaleDict]
-    );
-
-    if (!scaleDict) return null;
-
-    console.log(
-      "%c render ",
-      "background: orange; color: black; border-radius: 4px",
-      `<${Plot.name} ${innerProps.id || ""}>`
-    );
-
-    return <Plot {...innerProps} scale={scale} />;
-  }
-);
+export const PassScaleByID = connect(() => {
+  const scaleSelector = makeSelectScaleByID();
+  return (state, props) => ({
+    scaleDict: scaleSelector(state, props),
+  });
+})(ForwardScale);
