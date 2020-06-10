e6583a613b32f6036fd8a600ee25766530ed90a4
Foggyhazel <foggyhazel@outlook.com>
2020-06-10 16:52:04 +0700

    render only line chart for now

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { StyleSheet, View, ScrollView, SafeAreaView } from "react-native";
-import SimpleChart2 from "./src/examples/SimpleChart2";
-import TestStack from "./src/examples/TestStack";
+
 import TestLine from "./src/examples/TestLine";
-import TestHousePrice from "./src/examples/TestHousePrice";
-import TestBar from "./src/examples/TestBar";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <SafeAreaView>
         <ScrollView>
-          {/*<TestStack />
-          <SimpleChart2 />
           <TestLine />
-          <TestHousePrice />*/}
-          <TestBar />
         </ScrollView>
       </SafeAreaView>
     </View>
