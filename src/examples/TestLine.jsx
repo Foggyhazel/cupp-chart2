92a5e9cf31362f7a87b904117aae28903dba7a66
Foggyhazel <foggyhazel@outlook.com>
2020-07-01 23:06:45 +0700

    add grid cycling

--- src/examples/TestLine.jsx
+++ src/examples/TestLine.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import covid from "./data/covid.json";
 import { timeParse, timeFormat } from "d3-time-format";
 import { timeMonth } from "d3-time";
 import Chart from "../components/Chart";
 
 import LinePlot from "../components/LinePlot";
-import { View } from "react-native";
+import { View, Button } from "react-native";
 import Axis from "../components/Axis";
 import Grid from "../components/Grid";
 
@@ -19,14 +19,16 @@ const tay = [6];
 const tickFormat = timeFormat("%b");
 
 export default function TestLine() {
+  const [grid, setGrid] = useState(0);
   return (
     <View>
       <Chart data={mock} x={xa}>
-        <Grid Y />
+        <Grid X={grid == 1 || grid == 3} Y={grid == 2 || grid == 3} />
         <LinePlot y={cols} />
         <Axis X orient="bottom" tickArguments={ta} tickFormat={tickFormat} />
         <Axis Y tickArguments={tay} />
       </Chart>
+      <Button title="cycle Grid" onPress={() => setGrid((p) => (p + 1) % 4)} />
     </View>
   );
 }
