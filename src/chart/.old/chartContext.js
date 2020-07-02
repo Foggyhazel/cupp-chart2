1c8a9d905e50dc4d149b153f90118b5b5f87f366
Foggyhazel <foggyhazel@outlook.com>
2020-07-02 23:13:35 +0700

    removed unused files

--- src/chart/.old/chartContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const ChartContext = createContext();
-
-export const ChartContextProvider = ChartContext.Provider;
-
-export const useChartContext = () => {
-  return useContext(ChartContext);
-};
