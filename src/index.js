d0188d2ed436a9d5c804a25a5dc1ac907918f58d
Foggyhazel <foggyhazel@outlook.com>
2020-07-02 18:36:11 +0700

    add component index file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export { default as Chart } from "./components/Chart";
+
+export { default as LinePlot } from "./components/LinePlot";
+export { default as BarPlot } from "./components/BarPlot";
+export { default as AreaPlot } from "./components/AreaPlot";
+export { default as ScatterPlot } from "./components/ScatterPlot";
+
+export { default as Grid } from "./components/Grid";
+export { default as Axis } from "./components/Axis";
+export { default as Marker } from "./components/Marker";
+
+export { default as NoData } from "./components/NoData";
