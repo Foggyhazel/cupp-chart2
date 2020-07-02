023efc0c7440f3d39d5044dbe889a394eac1c364
Foggyhazel <foggyhazel@outlook.com>
2020-07-02 18:36:44 +0700

    add import alias

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,25 @@
 const { getDefaultConfig } = require("metro-config");
+const path = require("path");
 
 module.exports = (async () => {
   const {
     resolver: { assetExts },
   } = await getDefaultConfig();
   return {
+    watchFolders: [path.resolve(__dirname, "../relative/path/to/local/lib")],
     resolver: {
       assetExts: [...assetExts, "csv"],
+      extraNodeModules: new Proxy(
+        {},
+        {
+          get: (target, name) => {
+            if (target.hasOwnProperty(name)) {
+              return target[name];
+            }
+            return path.join(process.cwd(), `node_modules/${name}`);
+          },
+        }
+      ),
     },
   };
 })();
